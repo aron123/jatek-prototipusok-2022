6cfc0f41a7bac036b17f1cc2e4a58b32c7a9b4a4
Kiss, Aron <aron123dev@gmail.com>
2022-10-06 16:41:28 +0200

    Add lesson 5

--- 04-platformer-game/js/main.js
+++ 04-platformer-game/js/main.js
@@ -25,6 +25,8 @@ gameScene.preload = function () {
         margin: 1,
         spacing: 1
     });
+
+    this.load.json('levelData', 'assets/jsons/levelData.json');
 };
 
 gameScene.create = function () {
@@ -34,24 +36,29 @@ gameScene.create = function () {
         frameRate: 7
     });
 
-    const ground = this.add.sprite(0, 560, 'ground').setOrigin(0);
-    this.physics.add.existing(ground, true);
+    this.anims.create({
+        key: 'burn',
+        frames: this.anims.generateFrameNames('fire', { frames: [0, 1] }),
+        frameRate: 4,
+        repeat: -1
+    });
 
-    const blockWidth = this.textures.get('block').get(0).width;
-    const blockHeight = this.textures.get('block').get(0).height;
+    this.setupLevel();
 
-    const platform = this.add.tileSprite(50, 500, 4 * blockWidth, blockHeight, 'block');
-    platform.setOrigin(0);
+    this.setupSpawner();
 
-    this.physics.add.existing(platform, true);
-
-    this.player = this.add.sprite(300, 530, 'player', 3);
-    this.physics.add.existing(this.player);
-    this.player.body.setCollideWorldBounds();
-
-    this.physics.add.collider(this.player, [ground, platform]);
+    this.physics.add.collider(this.player, [this.platforms]);
+    this.physics.add.collider(this.goal, [this.platforms]);
+    this.physics.add.collider(this.barrels, [this.platforms]);
+    this.physics.add.overlap(this.player, [this.fires, this.goal, this.barrels], () => this.restartGame());
 
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.input.on('drag', (pointer, gameObject, x, y) => {
+        gameObject.x = x;
+        gameObject.y = y;
+        console.log(x, y);
+    });
 };
 
 gameScene.update = function () {
@@ -81,6 +88,88 @@ gameScene.update = function () {
         this.player.body.setVelocityY(this.jumpSpeed);
         this.player.setFrame(2);
     }
+};
+
+gameScene.setupLevel = function () {
+    this.levelData = this.cache.json.get('levelData');
+
+    this.platforms = this.physics.add.staticGroup();
+    for (const platform of this.levelData.platforms) {
+        let newPlatform;
+
+        if (platform.numTiles === 1) {
+            newPlatform = this.add.sprite(platform.x, platform.y, platform.key);
+        } else {
+            const blockWidth = this.textures.get(platform.key).get(0).width;
+            const blockHeight = this.textures.get(platform.key).get(0).height;
+            newPlatform = this.add.tileSprite(platform.x, platform.y, platform.numTiles * blockWidth, blockHeight, platform.key);
+        }
+
+        newPlatform.setOrigin(0);
+        this.platforms.add(newPlatform);
+    }
+
+    this.fires = this.physics.add.staticGroup();
+    for (const fire of this.levelData.fires) {
+        let newFire = this.add.sprite(fire.x, fire.y, 'fire');
+        newFire.setInteractive();
+        this.input.setDraggable(newFire);
+
+        newFire.setOrigin(0);
+        newFire.anims.play('burn');
+        this.fires.add(newFire);
+    }
+
+    this.goal = this.add.sprite(this.levelData.goal.x, this.levelData.goal.y, 'goal');
+    this.goal.setInteractive();
+    this.input.setDraggable(this.goal);
+    this.physics.add.existing(this.goal);
+
+    this.player = this.add.sprite(this.levelData.player.x, this.levelData.player.y, 'player', 3);
+    this.physics.add.existing(this.player);
+    this.player.body.setCollideWorldBounds();
+
+    this.physics.world.bounds.width = this.levelData.world.width;
+    this.physics.world.bounds.height = this.levelData.world.height;
+    this.cameras.main.setBounds(0, 0, this.physics.world.bounds.width, this.physics.world.bounds.height);
+    this.cameras.main.startFollow(this.player);
+};
+
+gameScene.restartGame = function () {
+    this.cameras.main.fade(500);
+    this.cameras.main.on('camerafadeoutcomplete', () => {
+        this.scene.restart();
+    });
+};
+
+gameScene.setupSpawner = function () {
+    this.barrels = this.physics.add.group({
+        collideWorldBounds: true,
+        bounceX: 1,
+        bounceY: 0.1
+    });
+
+    this.time.addEvent({
+        delay: this.levelData.spawner.interval,
+        loop: true,
+        callback: () => {
+            const barrel = this.barrels.get(this.levelData.goal.x, this.levelData.goal.y, 'barrel');
+
+            barrel.setVisible(true);
+            barrel.setActive(true);
+            barrel.body.enable = true;
+
+            barrel.body.setVelocityX(this.levelData.spawner.speed);
+
+            this.time.addEvent({
+                delay: this.levelData.spawner.lifespan,
+                callback: () => {
+                    this.barrels.killAndHide(barrel);
+                    barrel.body.enable = false;
+                }
+            });
+        }
+    });
 };
 
 const config = {
